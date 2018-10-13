4c99b994074f17ef63ad637cbf87096b3b120867
Gianine Dao <gianine.dao@sjsu.edu>
2018-10-13 15:22:13 -0700

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Neen on 10/13/18.
+//  Copyright © 2018 Gianine Dao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
